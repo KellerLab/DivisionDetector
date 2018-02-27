238f4958ed556ba1fef8f7251ef8fb7ad51a9b9b
Jan Funke <jfunke@iri.upc.edu>
2018-02-27 16:28:10 -0500

    Extend train script

--- 02_train/setup01/train.py
+++ 02_train/setup01/train.py
@@ -13,9 +13,22 @@ samples = [
 
 def train_until(max_iteration):
 
+    # Get the latest checkpoint.
+    if tf.train.latest_checkpoint('.'):
+        trained_until = int(tf.train.latest_checkpoint('.').split('_')[-1])
+    else:
+        trained_until = 0
+    if trained_until >= max_iteration:
+        return
+
+    with open('net_io_names.json', 'r') as f:
+        net_io_names = json.load(f)
+
     raw = ArrayKey('RAW')
     divisions = PointsKey('DIVISIONS')
     division_balls = ArrayKey('DIVISION_BALLS')
+    prediction = ArrayKey('DIVISION_PREDICTION')
+    loss_gradient = ArrayKey('DIVISION_PREDICTION')
 
     sources = tuple(
 
@@ -43,10 +56,35 @@ def train_until(max_iteration):
     pipeline = (
         sources +
         RandomProvider() +
+        # TODO:
+        # * augment
+        # * pre-cache
         RasterizePoints(
             divisions,
             division_balls,
             raster_settings=RasterizationSettings(ball_radius=20)) +
+        Train(
+            # The network to use.
+            'unet',
+            # Name of the optimzer.
+            optimizer=net_io_names['optimizer'],
+            # Name of the operator that computes the loss.
+            loss=net_io_names['loss'],
+            # Map inputs of the network to arrays we provide.
+            inputs={
+                net_io_names['raw']: raw,
+                net_io_names['gt_labels']: division_balls,
+            },
+            # Map outputs of the network to arrays that will be created by
+            # Train.
+            outputs={
+                net_io_names['labels']: prediction
+            },
+            # Map gradients of outputs wrt to the loss to arrays that will be
+            # created by Train.
+            gradients={
+                net_io_names['labels']: loss_gradient
+            }) +
         Snapshot({
             raw: 'volumes/raw',
             division_balls: 'volumes/divisions'
@@ -59,8 +97,13 @@ def train_until(max_iteration):
     request.add(raw, Coordinate((200, 200, 200)))
     request.add(division_balls, Coordinate((200, 200, 200)))
 
-    with build(pipeline):
-        pipeline.request_batch(request)
+    with build(train_pipeline) as b:
+
+        print("Starting training...")
+
+        # Request as many batches as the number of iterations.
+        for i in range(max_iteration - trained_until):
+            b.request_batch(request)
 
 if __name__ == "__main__":
 

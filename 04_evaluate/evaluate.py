7597511818d3a74994178a56bb49f1b3a44cffd1
Jan Funke <jfunke@iri.upc.edu>
2018-03-22 13:39:35 -0400

    Add detection and evaluation scripts

---
+++ 04_evaluate/evaluate.py
@@ -0,0 +1,141 @@
+from __future__ import print_function
+from scipy.optimize import linear_sum_assignment
+import json
+import sys
+import numpy as np
+
+# maximal distance in world units to consider a detection to match with a
+# ground-truth annotation
+matching_threshold = 100
+
+def create_matching_costs(rec_divisions, gt_divisions):
+
+    num_recs = len(rec_divisions)
+    num_gts = len(gt_divisions)
+
+    rec_labels = {
+        i: l
+        for i, l in enumerate(rec_divisions.keys())
+    }
+    gt_labels = {
+        i: l
+        for i, l in enumerate(gt_divisions.keys())
+    }
+
+    rec_locations = np.array(
+        [
+            rec_divisions[l]['center']
+            for l in rec_divisions.keys()
+        ], dtype=np.float32)
+    gt_locations = np.array(
+        [
+            gt_divisions[l]['center']
+            for l in gt_divisions.keys()
+        ], dtype=np.float32)
+
+    print(rec_locations)
+    print(gt_locations)
+
+    print("Computing matching costs...")
+    matching_costs = np.zeros((num_recs, num_gts), dtype=np.float32)
+
+    for i in range(num_recs):
+        for j in range(num_gts):
+
+            distance = np.linalg.norm(rec_locations[i] - gt_locations[j])
+
+            # ensure that pairs exceeding the matching threshold are not
+            # considered during the matching by giving them a very high cost
+            if distance > matching_threshold:
+                distance = 100*matching_threshold
+
+            matching_costs[i][j] = distance
+
+    return matching_costs, rec_labels, gt_labels
+
+def evaluate(rec_divisions, gt_divisions):
+
+    costs, rec_labels, gt_labels = create_matching_costs(
+        rec_divisions,
+        gt_divisions)
+
+    print("Finding cost-minimal matches...")
+    matches = linear_sum_assignment(costs - np.amax(costs) - 1)
+    matches = zip(matches[0], matches[1])
+
+    filtered_matches = [
+        (i,j, costs[i][j])
+        for (i,j) in matches
+        if costs[i][j] <= matching_threshold
+    ]
+
+    print("%d matches found"%len(filtered_matches))
+
+    # unmatched in rec = FP
+    fp = len(rec_divisions) - len(filtered_matches)
+
+    # unmatched in gt = FN
+    fn = len(gt_divisions) - len(filtered_matches)
+
+    # matched = TP
+    tp = len(filtered_matches)
+
+    precision = float(tp)/(tp + fp)
+    recall = float(tp)/(tp + fn)
+    fscore = 2.0*precision*recall/(precision + recall)
+
+    return (precision, recall, fscore, fp, fn)
+
+if __name__ == "__main__":
+
+    rec_file = sys.argv[1]
+    gt_file = sys.argv[2]
+    frame = int(sys.argv[3])
+
+    print("Evaluating frame %d"%frame)
+
+    with open(rec_file, 'r') as f:
+        rec_divisions = json.load(f)['divisions']
+
+    # DEBUG, DELME
+    for k, v in rec_divisions.items():
+        c = rec_divisions[k]['center']
+        rec_divisions[k]['center'] = ((c[0] - 360.0), c[1]/5.0, c[2])
+
+    print("Read %d rec divisions"%len(rec_divisions))
+
+    gt_divisions = {}
+    gt_label = 1
+    for line in open(gt_file, 'r'):
+
+        tokens = line.split()
+        if int(round(float(tokens[0]))) == frame:
+            center = tuple(float(x) for x in tokens[1:])
+            gt_divisions[gt_label] = {
+                'center': center
+            }
+            gt_label += 1
+
+    print("Read %d GT divisions"%len(gt_divisions))
+
+    precision, recall, fscore, fp, fn = evaluate(rec_divisions, gt_divisions)
+
+    result = {
+        'divisions': rec_divisions,
+        'scores': {
+            'precision': precision,
+            'recall': recall,
+            'f-score': fscore,
+            'fp': fp,
+            'fn': fn
+        }
+    }
+
+    with open(rec_file, 'w') as f:
+        json.dump(result, f)
+
+    print("precision: %f"%precision)
+    print("recall   : %f"%recall)
+    print("f-score  : %f"%fscore)
+    print("FPs      : %d"%fp)
+    print("FNs      : %d"%fn)
---
+++ 04_evaluate/find_divisions.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+from scipy.ndimage.filters import median_filter
+from scipy.ndimage import measurements, label
+import h5py
+import numpy as np
+import json
+import sys
+
+# the minimal value to predict for a voxel to be considered part of a blob
+blob_prediction_threshold = 0.01
+
+# the minimal size of a blob
+blob_size_threshold = 10
+
+def find_peaks(predictions):
+
+    # smooth out "U-Net noise"
+    # print("Median-filtering prediction...")
+    # predictions = median_filter(predictions, size=3)
+
+    print("Finding blobs...")
+    blobs = predictions > blob_prediction_threshold
+    labels, num_blobs = label(blobs)
+    print("Found %d blobs"%num_blobs)
+
+    print("Finding peaks, sizes, and maximal values...")
+    label_ids = np.arange(1, num_blobs + 1)
+    centers = measurements.center_of_mass(blobs, labels, index=label_ids)
+    sizes = measurements.sum(blobs, labels, index=label_ids)
+    maxima = measurements.maximum(predictions, labels, index=label_ids)
+
+    peaks = {
+        label: (center, max_value)
+        for label, center, size, max_value in zip(label_ids, centers, sizes, maxima)
+        if size >= blob_size_threshold
+    }
+
+    return (peaks, labels)
+
+def find_divisions(prediction_filename, thresholds, output_basenames):
+    '''Find all divisions in the predictions of a frame.'''
+
+    print("Reading predictions...")
+    with h5py.File(prediction_filename, 'r') as f:
+        ds = f['volumes/divisions']
+        predictions = np.array(ds[0])
+        offset = ds.attrs['offset']
+        resolution = ds.attrs['resolution']
+
+    print("Finding peaks...")
+    peaks, labels = find_peaks(predictions)
+
+    with h5py.File(prediction_filename, 'r+') as f:
+
+        if 'volumes/blobs' in f:
+            del f['volumes/blobs']
+
+        ds = f.create_dataset(
+            'volumes/blobs',
+            data=labels,
+            dtype=np.uint64,
+            compression='gzip')
+
+        ds.attrs['offset'] = offset
+        ds.attrs['resolution'] = resolution
+
+    print("Storing results...")
+    for threshold, outfile_basename in zip(thresholds, outfile_basenames):
+
+        threshold_peaks = {
+            label: {
+                'center': tuple(
+                    c*r+o
+                    for c, o, r
+                    in zip(center, offset[1:], resolution[1:])),
+                'max_value': float(max_value)
+            }
+            for label, (center, max_value) in peaks.items()
+            if max_value >= threshold
+        }
+
+        result = {
+            'divisions': threshold_peaks
+        }
+        with open(outfile_basename + '.json', 'w') as f:
+            json.dump(result, f, indent=2)
+
+if __name__ == "__main__":
+
+    args_file = sys.argv[1]
+    with open(args_file, 'r') as f:
+        args = json.load(f)
+    find_divisions(**args)

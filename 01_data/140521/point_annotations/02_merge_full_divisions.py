2c34663fd27e80fe643981e2834904465ba7ea36
Jan Funke <jfunke@iri.upc.edu>
2018-04-16 12:45:15 -0400

    Sort annotation consolidation scripts by run-order

---
+++ 01_data/140521/point_annotations/02_merge_full_divisions.py
@@ -0,0 +1,7 @@
+frames = [100, 120, 240, 250, 360, 400]
+
+with open('full_divisions.txt', 'w') as f:
+    for frame in frames:
+        for line in open('full_divisions_t=%d.txt'%frame, 'r'):
+            z, y, x = [ int(float(t)) for t in line.split() ]
+            f.write('%d\t%d\t%d\t%d\n'%(frame, z, y, x))
